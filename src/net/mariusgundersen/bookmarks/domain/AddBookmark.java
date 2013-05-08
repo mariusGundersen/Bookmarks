25470e4324a3e3242ad69af8af3aefaceb42dc07
Marius Gundersen <gundersen@gmail.com>
2013-05-08 19:06:28 +0200

    Added constraints to the command got a newer version of Ordnung.js, with better support for constraints

--- src/net/mariusgundersen/bookmarks/domain/AddBookmark.java
+++ src/net/mariusgundersen/bookmarks/domain/AddBookmark.java
@@ -1,9 +1,18 @@
 package net.mariusgundersen.bookmarks.domain;
 
+import javax.validation.constraints.Pattern;
+import org.apache.bval.constraints.NotEmpty;
 import qvc.executables.Command;
 
 public class AddBookmark extends Command {
+	
+
+	@NotEmpty(message="You must enter a url")
+	@Pattern(regexp="^https://.*", flags = {Pattern.Flag.CASE_INSENSITIVE, Pattern.Flag.MULTILINE}, message="The url must start with http(s)://")
 	public final String url;
+	
+	
+	@NotEmpty(message="You must enter a name for the bookmark")
 	public final String name;
 	
 	public AddBookmark(String url, String name) {

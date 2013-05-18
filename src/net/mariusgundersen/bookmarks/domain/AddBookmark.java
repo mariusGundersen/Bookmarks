915fb7b5d4e83cccef4e993c75770d6edff6f0b9
Marius Gundersen <gundersen@gmail.com>
2013-05-18 17:00:01 +0200

    Added constraints

--- src/net/mariusgundersen/bookmarks/domain/AddBookmark.java
+++ src/net/mariusgundersen/bookmarks/domain/AddBookmark.java
@@ -1,10 +1,18 @@
 package net.mariusgundersen.bookmarks.domain;
 
+import javax.validation.constraints.Pattern;
+
+import org.apache.bval.constraints.NotEmpty;
+
 import qvc.executables.Command;
 
 public class AddBookmark extends Command {
 
+	@NotEmpty(message="You must entere a title")
 	public final String title;
+	
+	@NotEmpty(message="You must enter a url")
+	@Pattern(regexp = "http://.*", message="The url must start with http://")
 	public final String url;
 	
 	
